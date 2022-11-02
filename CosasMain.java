3992e48d0119ca289c6e8616f18a65d671869314
JulianLeira <92724165+JulianLeira@users.noreply.github.com>
2022-11-02 15:25:08 +0100

    Update CosasMain.java

--- CosasMain.java
+++ CosasMain.java
@@ -5,6 +5,7 @@ public class CosasMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Buenas tarde");
+		System.out.println("Buenas tarde");
 		
 		
 
